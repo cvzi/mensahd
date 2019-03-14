35eadba3fdf0ed50b6f9716591ee6baf10b16893
cvzi <cuzi@openmail.cc>
2019-03-14 22:16:42 +0100

    local.py

---
+++ local.py
@@ -0,0 +1,10 @@
+#
+# For testing only
+#
+if __name__ == '__main__':
+    
+    from mensahd import wsgi
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 80, wsgi.application)
+    print("http://localhost:80/")
+    httpd.serve_forever()
