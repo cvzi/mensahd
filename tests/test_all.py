6ee2b9a0ee6d81c6f7cb4df990ff19fc215eebee
cvzi <cuzi@openmail.cc>
2020-08-29 21:43:55 +0200

    coverage

--- tests/test_all.py
+++ tests/test_all.py
@@ -116,7 +116,7 @@ def check_xml(parser, canteen):
 
 
 def test_all_modules():
-    moduleNames = ['eppelheim', 'heidelberg', 'koeln', 'mannheim', 'stuttgart']
+    moduleNames = ['eppelheim', 'heidelberg', 'koeln', 'mannheim', 'stuttgart', 'ulm']
 
     print("Importing %s" % (", ".join(moduleNames), ), end="")
 
